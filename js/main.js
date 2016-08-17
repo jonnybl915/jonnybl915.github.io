8ce0ce61f5de728cade6470df6c627b5153afc2d
jonathandavidblack <jonnybl915@gmail.com>
2016-08-17 16:37:52 -0400

    added jquery to toggle down arrow visibility when scrolling

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ $(document).ready(function(){
     	alert('asdf');
     }
   });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 425) { 
+        $('.blink-img').css({
+            'visibility': 'hidden'
+        });
+    }
+    else {
+    	$('.blink-img').css({
+            'visibility': 'visible'
+        });
+    }
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1500) { 
+        $('.blink-img-2').css({
+            'visibility': 'hidden'
+        });
+    }
+    else {
+    	$('.blink-img-2').css({
+    		'visibility': 'visible'
+    	})
+    }
+});
